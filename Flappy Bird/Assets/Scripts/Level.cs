5618aa752e25493222f925241d239bc1ddcb62ea
YarikTalko <ariktalko@gmail.com>
2023-06-13 22:48:34 +0300

    pipe movement

--- Flappy Bird/Assets/Scripts/Level.cs
+++ Flappy Bird/Assets/Scripts/Level.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Level : MonoBehaviour
@@ -5,12 +6,41 @@ public class Level : MonoBehaviour
     private const float CAMERA_ORTHO_SIZE = 50f;
     private const float PIPE_WIDTH = 7.8f;
     private const float PIPE_HEAD_HEIGHT = 3.75f;
+    private const float PIPE_MOVE_SPEED = 3f;
+    private const float PIPE_DESTROY_X_POSITION = -130f;
+
+    private List<Pipe> pipeList;
+
+    private void Awake()
+    {
+        pipeList = new List<Pipe>();
+    }
 
     private void Start()
     {
         //CreatePipe(50f, 20f, true);
         //CreatePipe(50f, 20f, false);
         CreateGapPipes(50f, 20f, 20f);
+    }
+
+    private void Update()
+    {
+        HandlePipeMovement();
+    }
+
+    private void HandlePipeMovement()
+    {
+        for (int i = 0; i < pipeList.Count; i++)
+        {
+            Pipe pipe = pipeList[i];
+            pipe.Move();
+            if (pipe.GetXPosition() < PIPE_DESTROY_X_POSITION)
+            {
+                pipe.DestroySelf();
+                pipeList.Remove(pipe);
+                i--;
+            }
+        }
     }
 
     private void CreateGapPipes(float gapY, float gapSize, float xPosition)
@@ -52,5 +82,37 @@ public class Level : MonoBehaviour
         BoxCollider2D pipeBodyBoxCollider = pipeBody.GetComponent<BoxCollider2D>();
         pipeBodyBoxCollider.size = new Vector2(PIPE_WIDTH, height);
         pipeBodyBoxCollider.offset = new Vector2(0f, height * .5f);
+
+        Pipe pipe = new Pipe(pipeHead, pipeBody);
+        pipeList.Add(pipe);
+    }
+
+    private class Pipe
+    {
+        private Transform pipeHeadTransform;
+        private Transform pipeBodyTransform;
+
+        public Pipe(Transform pipeHeadTransform, Transform pipeBodyTransform)
+        {
+            this.pipeHeadTransform = pipeHeadTransform;
+            this.pipeBodyTransform = pipeBodyTransform;
+        }
+
+        public void Move()
+        {
+            pipeHeadTransform.position += new Vector3(-1, 0, 0) * PIPE_MOVE_SPEED * Time.deltaTime;
+            pipeBodyTransform.position += new Vector3(-1, 0, 0) * PIPE_MOVE_SPEED * Time.deltaTime;
+        }
+
+        public float GetXPosition()
+        {
+            return pipeHeadTransform.position.x;
+        }
+
+        public void DestroySelf()
+        {
+            Destroy(pipeHeadTransform.gameObject);
+            Destroy(pipeBodyTransform.gameObject);
+        }
     }
 }
